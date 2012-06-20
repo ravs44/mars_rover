a565717ea14e3d8bbc70b884b186110bff063d43
Ravinder <ravinder.singh@greenbackforex.com>
2012-06-20 18:37:53 +0530

    simple crud functionality (except new) working

--- web/app.rb
+++ web/app.rb
@@ -1,7 +1,35 @@
 require 'sinatra'
 require './marsrover.rb'
 
+class NotFound < Exception; end
+
+
 get '/rovers' do
   @rovers = Rover.all
   haml :index
 end
+
+
+get '/rovers/:id' do
+  @rover = Rover.get(params[:id])
+  raise NotFound unless @rover
+  haml :show
+end
+
+
+get '/rovers/:id/edit' do
+  @rover = Rover.get(params[:id])
+  haml :edit
+end
+
+put '/rovers/:id' do
+  @rover = Rover.get(params[:id])
+  raise NotFound unless @rover
+  @rover.update(params[:rover])
+  if @rover.save
+    redirect "/rovers"
+  else
+    haml :edit
+  end
+end
+
